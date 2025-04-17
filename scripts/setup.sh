584e1e0073e1a4066d4d145200b2db1f4194dbc7
Mohammad Alghzawi <mohd.a.ghzawi@gmail.com>
2025-04-18 02:04:36 +0300

    setup changes

---
+++ scripts/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "Updating system"
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Git"
+sudo apt install git -y
+
+echo "Installing Docker"
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "Installing Docker Compose"
+sudo apt install docker-compose -y
+
+echo "Adding user to docker group"
+sudo usermod -aG docker $USER
+
+echo "Cloning repository"
+ git clone https://github.com/Mohammad-alghzawi/devops-challenge.git
+
+echo "Starting Docker containers"
+docker-compose up -d
+
+
